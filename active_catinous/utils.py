3c53570c0d4e66f1b3ef8c2ed926d16057cd853c
Matthias Perkonigg <e1129269@student.tuwien.ac.at>
2020-11-04 11:05:21 +0100

    bug fix

--- active_catinous/utils.py
+++ active_catinous/utils.py
@@ -84,7 +84,7 @@ def sort_dict(input_dict):
 def save_memory_to_csv(memory, savepath):
     df_cache = pd.DataFrame({'filepath':[ci.filepath for ci in memory],
                              'label': [ci.label.cpu().numpy() for ci in memory],
-                             'res': [ci.res for ci in memory],
+                             'scanner': [ci.scanner for ci in memory],
                              'pseudo_domain':  [ci.pseudo_domain for ci in memory],
                              'traincounter': [ci.traincounter for ci in memory]})
     df_cache.to_csv(savepath, index=False, index_label=False)
