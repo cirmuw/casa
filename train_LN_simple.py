e0ce9491369bd3d442500c5f4aca68be8e4b7a85
mperkonigg <matthias.perkonigg@entenbaer.at>
2020-11-24 14:22:21 +0100

    simple training added

--- train_LN_simple.py
+++ train_LN_simple.py
@@ -5,8 +5,23 @@ from datasets.MDTLUNADataset import MDTLUNADataset
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import numpy as np
+import models.MDTRetinaNet as mdtr
 
-def train_loop(model, dl, optimizer, savepath, scheduler=None, epochs=50):
+
+def train_loop(ds_path, ds_split, savepath, lr=1e-4, scheduler_steps=10, n_slices=1, scheduler=None, epochs=50):
+    cf = mdtr.config()
+    logger = logging.getLogger('medicaldetectiontoolkit')
+    logger.setLevel(logging.DEBUG)
+    model = mdtr.net(cf, logger)
+
+    ds = MDTLUNADataset(ds_path, split=ds_split, n_slices=n_slices)
+    dl = DataLoader(ds, batch_size=4, num_workers=4, shuffle=True)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    if scheduler_steps is not None:
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_steps, gamma=0.1)
+    else:
+        scheduler = None
+
     for epoch in range(epochs):
         running_loss = 0.0
         running_class_loss = 0.0
